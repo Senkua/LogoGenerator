ee80abaaaac6070b88f4891a29a60b1decac9151
Liam <liam64575@icloud.com>
2023-06-25 16:47:51 -0400

    Debugging logo generation, and adding better stylization

--- logo_generator.js
+++ logo_generator.js
@@ -1,64 +1,56 @@
-const readline = require('readline');
+const { SVG, registerWindow } = require('@svgdotjs/svg.js');
 const fs = require('fs');
-const svg2png = require('svg2png');
-const svgpath = require('svgpath');
-const { createCanvas } = require('canvas');
+const { createSVGWindow } = require('svgdom');
 
-const rl = readline.createInterface({
+// Create a DOM environment for SVG rendering
+const window = createSVGWindow();
+const document = window.document;
+registerWindow(window, document);
+
+const canvas = SVG(document.documentElement).size(300, 200);
+
+const rl = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function generateLogo() {
-  rl.question('Enter up to three characters for the logo: ', (text) => {
-    rl.question('Enter the text color (keyword or hexadecimal number): ', (textColor) => {
-      const shapes = ['circle', 'triangle', 'square'];
-      console.log('Choose a shape:');
-      shapes.forEach((shape, index) => {
-        console.log(`${index + 1}. ${shape}`);
-      });
-      rl.question('Enter the number corresponding to the desired shape: ', (shapeChoice) => {
-        const shape = shapes[shapeChoice - 1];
-        rl.question('Enter the shape color (keyword or hexadecimal number): ', (shapeColor) => {
-          const canvas = createCanvas(300, 200);
-          const ctx = canvas.getContext('2d');
-          ctx.fillStyle = shapeColor;
-          ctx.fillRect(0, 0, 300, 200);
-          ctx.fillStyle = textColor;
-          
-          if (shape === 'circle') {
-            ctx.beginPath();
-            ctx.arc(150, 100, 50, 0, 2 * Math.PI);
-            ctx.fill();
-          } else if (shape === 'triangle') {
-            ctx.beginPath();
-            ctx.moveTo(100, 100);
-            ctx.lineTo(200, 100);
-            ctx.lineTo(150, 25);
-            ctx.closePath();
-            ctx.fill();
-          } else if (shape === 'square') {
-            ctx.fillRect(100, 50, 100, 100);
-          }
-          
-          ctx.fillStyle = 'white';
-          ctx.font = '50px sans-serif';
-          ctx.fillText(text, 135, 130);
-          
-          const svg = svgpath(canvas.toBuffer().toString('utf-8'))
-            .scale(1, -1)
-            .translate(0, -200)
-            .toString();
-          
-          fs.writeFileSync('logo.svg', svg);
-          svg2png.sync('logo.svg', 'logo.png');
-          
-          console.log('Generated logo.svg');
-          rl.close();
-        });
+rl.question('Enter the text (up to 3 characters): ', (text) => {
+  rl.question('Enter the text color (keyword or hexadecimal): ', (textColor) => {
+    rl.question('Choose a shape (circle, triangle, square): ', (shape) => {
+      rl.question('Enter the shape color (keyword or hexadecimal): ', (shapeColor) => {
+        rl.close();
+
+        // Set the text attributes
+        const textElement = canvas.text(text).move(50, 100).fill(textColor);
+
+        // Set the shape attributes
+        let shapeElement;
+        switch (shape) {
+          case 'circle':
+            shapeElement = canvas.circle(100);
+            break;
+          case 'triangle':
+            shapeElement = canvas.polygon('150,30 75,170 225,170');
+            break;
+          case 'square':
+            shapeElement = canvas.rect(150, 150);
+            break;
+          default:
+            console.log('Invalid shape. Exiting...');
+            return;
+        }
+        shapeElement.move(75, 25).fill(shapeColor);
+
+        // Add the text and shape to the canvas
+        canvas.add(textElement);
+        canvas.add(shapeElement);
+
+        // Export the SVG to a file
+        const svgString = canvas.svg();
+        fs.writeFileSync('logo.svg', svgString);
+
+        console.log('Generated logo.svg');
       });
     });
   });
-}
-
-generateLogo();
+});
